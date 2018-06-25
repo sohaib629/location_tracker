3cdf47b79464ce6175bac3b84762b2f32a324c79
tom <tom@example.com>
2018-06-25 19:41:12 +0000

    file is deleted

--- paytm.java
+++ paytm.java
@@ -1,5 +1,6 @@
 Adding some new class for some purpose
 By Tom
 
-modifications by jerry 
+modifications by jerry
+Adding some content 
 Adding a new function which will detect driver details when notification come from driver app
